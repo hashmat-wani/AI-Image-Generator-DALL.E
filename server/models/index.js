a7b94cde238f1d92e9d3006669bb66669d49f4ee
Hashmat-Noorani <hashmatwani@icloud.com>
2023-02-08 00:41:53 +0530

    testing...

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,3 @@
 export { default as Post } from "./post.model.js";
 export { default as User } from "./user.model.js";
-export { default as RefreshToken } from "./refreshToken.model.js";
 export { default as OTPVerification } from "./otpVerification.model.js";
