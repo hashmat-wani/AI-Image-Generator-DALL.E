170c3e9d6ed0aff67d248e906c35b5609464a112
Hashmat-Noorani <hashmatwani@icloud.com>
2023-02-20 15:04:57 +0530

    tetsing

--- client/src/utils/downloadImg.js
+++ client/src/utils/downloadImg.js
@@ -3,5 +3,6 @@ import FileSaver from "file-saver";
 export async function downloadImage({ id, url }) {
   url = url.split("/");
   url[url.length - 2] = "fl_attachment";
+  console.log(id, url.join("/"));
   FileSaver.saveAs(url.join("/"), `dallE-${id}`);
 }
